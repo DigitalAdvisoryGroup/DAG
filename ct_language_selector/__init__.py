65097ecfa4fc24c845e77b2a581ea4a07bce3370
DigitalAdvisoryGroup <git@digitaladvisorygroup.io>
2020-05-14 22:07:46 +0200

    Webpage language selector

---
+++ ct_language_selector/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+#nothing to point to
---
+++ ct_language_selector/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# © 2019 Piotr Cierkosz <info@cier.tech>
+{
+    'name': 'Language Selector in the top menu',
+    'category': 'Website',
+    'summary': 'Adds language selector to the top menu',
+    'author': 'Piotr Cierkosz',
+    'website': 'http://www.cier.tech',
+    'version': '1.0',
+    'description': '''
+Adds language selector to the top menu
+''',
+    'depends': [
+        'website',
+    ],
+    'data': [
+        'templates/website.xml',
+    ],
+    'installable': True,
+    'price': 10.0,
+    'currency': 'EUR',
+    "installable": True,
+    "application": False,
+    'license': 'Other proprietary',
+    'images': ['images/thumbnail.png'],
+}
