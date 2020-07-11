4ca276cdb590fa5c7acb054c8db41b31aa076bcf
Digital Advisory Group <git@digitaladvisorygroup.io>
2020-07-11 23:06:02 +0200

    Add files via upload
    Google Vision API integration added

---
+++ website_vision_snippet/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Website Vision Snippet",
+    'summary': """Face vision api""",
+    'description': """""",
+    'author': "Kanak Infosystems",
+    'category': 'Website',
+    'depends': ['website'],
+    'data': [
+        'wizard/vision_views.xml',
+        'views/templates.xml',
+        'views/google_vision_views.xml',
+    ],
+    'images': [
+    ],
+    'installable': True,
+}
+
+# Pre Install
+# $ sudo pip3 install google-cloud
+# $ sudo pip3 install google-cloud-vision
